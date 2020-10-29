1b0d88ab028ae574d671095c3d626e199c9ea537
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-10-29 17:57:46 +0000

    Updated to add constant and changeed rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -50,7 +50,7 @@ describe DockingStation do
     it 'raises an error when the dock is full' do
       # bike = Bike.new
       # subject.dock(bike)
-      20.times { subject.dock(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Dock Full'
     end
   end
