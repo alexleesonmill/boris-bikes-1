f50b879c046e333f5241b4857063571523672854
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-10-29 10:27:40 +0000

    Refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,24 +1,14 @@
 require 'docking_station'
-require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'gets a Bike instance' do
-    expect(subject.release_bike).not_to eq nil
-  end
-
-  describe '#working?' do
-    it 'responds to the working? method of the Bike class' do
-      expect(subject.release_bike).to respond_to(:working?)
-    end
-  end
-
   it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
 
   it 'docks something' do
     bike = Bike.new
-    # We want to return the bike we dock
     expect(subject.dock(bike)).to eq bike
   end
 
@@ -27,9 +17,18 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
-end
-# responds to release_bike" do
-# expect(subject).to respond_to :release_bike
 
-# it "release_bike returns a bike" do
-#  expect(DockingStation.new.release_bike).is_a?(Bike)
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+end
+# it "responds to release_bike" do
+# expect(subject).to respond_to :release_bike
