f3b48c919202c152b0f20e4225ca67b2db3c12d7
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-10-29 09:16:02 +0000

    Updated to chapter 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,32 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it { expect(DockingStation.new.release_bike).is_a?(Bike) }
-  it { expect(Bike.new.working?).to eq(true) }
-  it { is_expected.to respond_to :dock }
-  it 'checks if dock_bike has docked a bike' do
-    bike = subject.release_bike
-    expect(subject.dock(bike)).is_a?(Bike)
+
+  it 'gets a Bike instance' do
+    expect(subject.release_bike).not_to eq nil
+  end
+
+  describe '#working?' do
+    it 'responds to the working? method of the Bike class' do
+      expect(subject.release_bike).to respond_to(:working?)
+    end
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks something' do
+    bike = Bike.new
+    # We want to return the bike we dock
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
-
-# it "responds to release_bike" do
+# responds to release_bike" do
 # expect(subject).to respond_to :release_bike
 
 # it "release_bike returns a bike" do
